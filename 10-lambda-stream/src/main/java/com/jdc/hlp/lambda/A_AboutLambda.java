4f9a4eaa6f38a0dcf5fdaeb12a1c18b64fd3ab3d
hsulae-phyo <hsu.lae.2020.2021.fb@gmail.com>
2026-06-02 09:43:31 +0630

    lambda

--- 10-lambda-stream/src/main/java/com/jdc/hlp/lambda/A_AboutLambda.java
+++ 10-lambda-stream/src/main/java/com/jdc/hlp/lambda/A_AboutLambda.java
@@ -1,5 +1,26 @@
 package com.jdc.hlp.lambda;
 
 public class A_AboutLambda {
+	public static void main(String[] args) {
+		beforeLambda();
+	}
+	
+	static void afterLambda() {
+		Order order = () -> System.out.println("Checkout with lambda.");
+		order.checkout();
+	}
+	
+	static void beforeLambda() {
+		Order order = new Order() {
+			@Override
+			public void checkout() {
+				System.out.println("Checkout with lambda.");
+			}
+		};
+		order.checkout();
+	}
+}
 
+interface Order{
+	void checkout();
 }
